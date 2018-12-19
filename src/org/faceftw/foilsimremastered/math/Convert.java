23e7c9a4a59a675af5bbe9c5a19b1ad1ccc8d133
FaceFTW <FaceFTW@outlook.com>
2018-12-19 08:07:10 -0500

    Continued work on Convert.java
    My english teacher said I should work on the essay. Real programming
    hours bois

--- src/org/faceftw/foilsimremastered/math/Convert.java
+++ src/org/faceftw/foilsimremastered/math/Convert.java
@@ -12,43 +12,68 @@ import org.faceftw.foilsimremastered.Constants.Planet;
 
 public class Convert {
 	
+	public enum Units{FREEDOM,METRIC}
 	
 	//Some conversion factors
-	private static final double d2r = Math.PI/180.;				//Degrees to Radians
-	private static final double pid2 = Math.PI/2.0;				//Unknown at the moment
+	private static final double D2R = Math.PI/180.;					//Degrees to Radians
+	private static final double PID2 = Math.PI/2.0;					//Unknown at the moment
 	
 	//For Freedom Units given input data
-	private static final double freedom_length_mod = .1; 		//Feet
-	private static final double freedom_velocity_mod=.6818;		//mph
-	private static final double freedom_velocity_max=250.;		//mph, Unknown purpose atm
+	private static final double FREEDOM_LENGTH_MOD  = 1;			//Feet
+	private static final double FREEDOM_VELOCITY_MOD =.6818;		//mph
+	private static final double FREEDOM_VELOCITY_MAX = 250.;		//mph, Unknown purpose atm
+	private static final double FREEDOM_WEIGHT_MOD = 1.0;			//lbs
+	private static final double FREEDOM_WEIGHT_MAX = 100000.;		//lbs
+	private static final double FREEDOM_WEIGHT_MAX_B = .5;			//lbs
+	private static final double FREEDOM_PRESSURE_MOD = 14.7;		//lb/(in^2)
+	
+	//For Metric Units given input data
+	private static final double METRIC_LENGTH_MOD = .3048;			//meters
+	private static final double METRIC_VELOCITY_MOD = 1.097;		//km/hr
+	private static final double METRIC_VELOCITY_MAX = 400.;			//km/hr
+	private static final double METRIC_WEIGHT_MOD = 4.448;			//Newtons
+	private static final double METRIC_WEIGHT_MAX = 500000.;		//Newtons
+	private static final double METRIC_WEIGHT_MAX_B = 2.5;			//Newtons
+	private static final double METRIC_PRESSURE_MOD = 101.3;		//kPa
 	
 	
+	//Mars Related Constants
+	//Freedom Units 
+	private static final double MARS_FREEDOM_VELOCITY_MAX = 50.;	//mph
 	
+	//Metric Units
+	private static final double MARS_METRIC_VELOCITY_MAX = 80.;		//km/hr
 
 	
 	//Converting Degrees to Radians
 	public static double DegtoRad(double in) {
-		return in*d2r;
+		return in*D2R;
 	}
 	
 	//Unknown Conversion, temporary placeholder
 	public static double convPid2(double in) {
-		return in*pid2;
+		return in*PID2;
 	}
 
-	public static double toFreedomLength(double in) {
-		return in/freedom_length_mod;
+	public static double convLength(double in,Units u) {
+		switch(u) {
+		case FREEDOM:
+			return in*FREEDOM_LENGTH_MOD;
+		case METRIC:
+			return in*
+		}
 	}
 	
-	public static double toFreedomVelocity(Planet p,double in) {
+	public static double convVelocity(Planet p,double in, Units u) {
 		double out = 0;
 		switch(p) {
 		case EARTH:
-			out = in/freedom_velocity_mod;
+			out = in/FREEDOM_VELOCITY_MOD;
 			break;
 		case MARS:
+			
 			break;
 		}
-		return in/freedom_velocity_mod;
+		return in/FREEDOM_VELOCITY_MOD;
 	}
 }
